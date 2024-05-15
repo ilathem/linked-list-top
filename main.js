fbe1f70a36ea7503555dc3afc6ba52e2dabbafb2
Isaiah Lathem <isaiahlathem@gmail.com>
2024-05-14 20:24:25 -0500

    toString and append

---
+++ main.js
@@ -0,0 +1,52 @@
+class LinkedList {
+  constructor() {
+    this.root = null;
+  }
+  toString() {
+    let string = ` ( ${this.root.value} )`;
+    let node = this.root;
+    while (node) {
+      string += ` -> ( ${node.next?.value ?? 'null'} )`; // optional chaining!
+      node = node.next
+    }
+    console.log(string);
+  }
+  append(value) {
+    console.log(`appending ${value}`);
+    if (this.root === null) {
+      this.root = new Node(value);
+    } else {
+      let node = this.root;
+      while (node) {
+        if (node.next) 
+          node = node.next;
+        else {
+          node.next = new Node(value);
+          return;
+        }
+      }
+    }
+  }
+  // preprend(value) {
+  //   if (this.root === null) {
+  //     this.root = new Node(value);
+  //   } else {
+  //     let node = this.root;
+  //     this.root = new Node(value)
+  //     this.root.next = node;
+  //     while (node.next) {
+  //       
+      
+}
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+const list = new LinkedList();
+list.append('a');
+list.append('b');
+list.append('c');
+list.toString();
